ac2675d2e4f614d1938f4842ba9bb5db7c8ca524
Albert Jonathan <albert.jonathan23@gmail.com>
2018-10-28 17:18:25 +0800

    recovery

--- app/routes.py
+++ app/routes.py
@@ -1,47 +1,59 @@
-from flask import render_template, flash,redirect, url_for, request
-from app import app, db
-from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
-from hashlib import md5
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
     posts = [
         {
-            'author' : {'username': 'John'},
-            'body' : 'Beautiful day in Portland!'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'author' : {'username': 'Susan'},
-            'body' : 'The Avengers movie was cool!'
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
+    return render_template('index.html', title='Home', posts=posts)
 
-    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return(redirect(url_for('login')))
-        login_user(user,remember=form.remember_me.data)
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,24 +69,17 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-<<<<<<< HEAD
-        {'author': user, 'body': 'Test post#1'},
-        {'author': user, 'body': 'Test post#2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
-
-'https://www.gravatar.com/avatar/' + md5(b'albert.jonathan23@gmail.com').hexdigest()
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
 
-from app.forms import EditProfileForm
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -92,6 +97,7 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
 
+
 @app.route('/follow/<username>')
 @login_required
 def follow(username):
@@ -107,6 +113,7 @@ def follow(username):
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
 
+
 @app.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
@@ -121,4 +128,3 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
->>>>>>> a02ee84bf11a846664a032fe284e67cef50dcc7f
