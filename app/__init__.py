7766084d9a5ceb7cf62026f9ad8398c30547c207
Albert Jonathan <albert.jonathan23@gmail.com>
2018-11-02 13:57:55 +0800

    Refactor+Remove lazy strings+added send message

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,63 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+from flask_babel import Babel, lazy_gettext as _l
+from flask import current_app
 
-from app import routes, models, errors
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# login = LoginManager(app)
+# login.login_view = 'login'
+# login.login_message = _l('Please log in to access this page.')
+# bootstrap = Bootstrap(app)
+# moment = Moment(app)
+# babel = Babel(app)
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page')
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app((app))
+    migrate.init_app(app,db)
+    login.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    #if not app.debug and not app.testing:
+
+    return app
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+
+from app import models
+
+
+
--- app/models.py
+++ app/models.py
@@ -24,6 +24,13 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    language = db.Column(db.String(5))
+
+    messages_sent = db.relationship('Message',foreign_keys='Message.sender_id',
+                                    backref='author', lazy = 'dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recepient_id',
+                                        backref='recipient',lazy='dynamic')
+    last_messsage_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -58,6 +65,9 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
+    def new_messages(self):
+        last_read_time = self.last_messsage_read_time or datetime(1900,1,1)
+        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
 
 @login.user_loader
 def load_user(id):
@@ -69,6 +79,16 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
+    def __rep__(self):
+        return '<Post {}>'.format(self.body)
+
+class Message(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Message {}>'.format(self.body)
